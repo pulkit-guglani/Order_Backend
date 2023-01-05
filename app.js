e0b371ab74d78628555e5bb09efd9ff52ef763b2
Pulkit <pulkitname@gmail.com>
2023-01-05 18:03:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "./nodeSetupFiles/node-express-course/02-express-tutorial/navbar-app/index.html"
+    )
+  );
+});
+app.get("/styles.css", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "./nodeSetupFiles/node-express-course/02-express-tutorial/navbar-app/styles.css"
+    )
+  );
+});
+app.get("about", (req, res) => {
+  res.send("This is about page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("server started");
+});
