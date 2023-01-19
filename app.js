6867b52d570dbe43d7c5f2b9637ab81cc60db3c6
Pulkit <pulkitname@gmail.com>
2023-01-19 20:55:11 +0530

    renaming

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5001;
 const route = require("./routes/routes");
-const connectDB = require("./db/connectDb");
+const connectDb = require("./db/connectDb");
 var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 try {
-  connectDB();
+  connectDb();
 } catch (e) {
   console.log(e.message);
 }
