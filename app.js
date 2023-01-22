ab7a9903bb1ddb74859c541da23361000f9f9c4c
Pulkit <pulkitname@gmail.com>
2023-01-22 21:56:53 +0530

    socket.io added

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const route = require("./routes/routes");
 const connectDb = require("./db/connectDb.js");
+const http = require("http");
 var cors = require("cors");
+var server = http.Server(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } }).of("/api");
+
 app.use(cors());
 app.use(express.json());
 try {
@@ -20,6 +24,18 @@ app.all("*", (req, res) => {
   res.status(404).send("Resource not found");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server started");
+});
+
+// app.listen(PORT, () => {
+//   console.log("server started");
+// });
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("orderAdded", (message) => {
+    console.log("SOCKET: New order added");
+    socket.broadcast.emit("updateOrders");
+  });
 });
